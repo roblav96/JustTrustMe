4e361a44735bb56c6a10fa535ea7b5d02ba495b3
Robert Laverty <roblav96@gmail.com>
2018-04-13 06:07:59 -0400

    JustTrustMe4-release.apk using default keystore

--- build.sh
+++ build.sh
@@ -2,3 +2,6 @@ APK_PATH="bin/JustTrustMe.apk"
 ./gradlew assembleRelease && cp app/build/outputs/apk/app-release-unsigned.apk $APK_PATH && signapk $APK_PATH
 
 #adb install -r bin/AndroidVTS.apk
+
+# zipalign -v -p 4 JustTrustMe4-release-unsigned.apk JustTrustMe4-release-unsigned-aligned.apk
+# jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/.android/debug.keystore -out JustTrustMe4-signed.apk JustTrustMe4-unsigned-aligned.apk androiddebugkey
